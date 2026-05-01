66d3398f9c9ea8b6c0b27461acf60b93cc80a2bd
Linakhenfri <linakhenfri2004@gmail.com>
2026-05-01 20:31:20 +0100

    Update index.js

--- models/index.js
+++ models/index.js
@@ -3,24 +3,19 @@ const sequelize = require('../config/database');
 const User = require('./User');
 const Order = require('./Order');
 const Product = require('./Product');
-const OrderItem = require('./OrderItem'); // ✅ استعملي model منفصل
-
-// ================= RELATIONS =================
+const OrderItem = require('./OrderItem');
 
 // User ↔ Order
-User.hasMany(Order, { foreignKey: 'userId', onDelete: 'CASCADE' });
+User.hasMany(Order, { foreignKey: 'userId' });
 Order.belongsTo(User, { foreignKey: 'userId' });
 
-// 🔥 Order ↔ Product (Many-to-Many)
-Order.belongsToMany(Product, {
-  through: OrderItem,
-  foreignKey: 'OrderId',
-});
+// Order ↔ OrderItem
+Order.hasMany(OrderItem);
+OrderItem.belongsTo(Order);
 
-Product.belongsToMany(Order, {
-  through: OrderItem,
-  foreignKey: 'ProductId',
-});
+// Product ↔ OrderItem
+Product.hasMany(OrderItem);
+OrderItem.belongsTo(Product);
 
 module.exports = {
   sequelize,
