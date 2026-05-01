b00628eac1b97bea825d8fb2ed10f5c679ca25e2
Linakhenfri <linakhenfri2004@gmail.com>
2026-05-01 20:16:21 +0100

    Update orderRoutes.js

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -10,8 +10,8 @@ router.post("/", auth, orderController.createOrder);
 // Get orders (IMPORTANT)
 router.get("/", auth, orderController.getOrders);
 
-<<<<<<< HEAD
+
 module.exports = router;
-=======
+
 module.exports = router;
->>>>>>> d9857561f7d27ebc749fa00ff03caf5e8ef47292
+
