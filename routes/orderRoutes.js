19767ebb3719dedec6ab662193441be88ba08744
Linakhenfri <linakhenfri2004@gmail.com>
2026-05-01 14:52:09 +0100

    fix root route

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -4,7 +4,10 @@ const router = express.Router();
 const auth = require("../middleware/auth");
 const orderController = require("../controllers/orderController");
 
-// ✅ Create order (protected route)
+// Create order
 router.post("/", auth, orderController.createOrder);
+
+// Get orders (IMPORTANT)
+router.get("/", auth, orderController.getOrders);
 
 module.exports = router;
