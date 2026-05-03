83941a6d3204575f224b380d26b86f8350dcd4c7
Linakhenfri <linakhenfri2004@gmail.com>
2026-05-03 09:13:00 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -5,28 +5,25 @@ const User = sequelize.define("User", {
   name: {
     type: DataTypes.STRING,
     allowNull: false,
-    validate: {
-      notEmpty: true
-    }
+    validate: { notEmpty: true }
   },
 
   email: {
     type: DataTypes.STRING,
     allowNull: false,
     unique: true,
-    validate: {
-      isEmail: true,
-      notEmpty: true
-    }
+    validate: { isEmail: true, notEmpty: true }
   },
 
   password: {
     type: DataTypes.STRING,
     allowNull: false,
-    validate: {
-      notEmpty: true,
-      len: [6, 100]
-    }
+    validate: { len: [6, 100] }
+  },
+
+  role: {
+    type: DataTypes.STRING,
+    defaultValue: "user"
   }
 });
 
